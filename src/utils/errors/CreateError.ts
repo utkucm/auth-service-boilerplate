d3e6ccca990261b4b66df7f18b4d2b9ad132ae0b
Utku Çam <utkucam2@gmail.com>
2021-06-05 21:37:23 +0300

    Adjust static methods according to seperated error classes

--- src/utils/errors/CreateError.ts
+++ src/utils/errors/CreateError.ts
@@ -2,38 +2,22 @@ import BadRequest from './BadRequest';
 import NotFound from './NotFound';
 import Unauthorized from './Unauthorized';
 import InternalServer from './InternalServer';
-import BaseError, { HttpStatusCode } from './BaseError';
 
 class CreateError {
-  public static BadRequestError = (message: string = 'Invalid Request.'): BaseError => {
+  public static BadRequestError = (message?: string): BadRequest => {
     return new BadRequest(message);
   };
 
-  public static UnauthorizedError = (
-    name: string = 'Unauthorized Error',
-    message: string = 'You are unauthorized.',
-    isOperational: boolean = true,
-    statusCode: HttpStatusCode = HttpStatusCode.UNAUTHORIZED
-  ): BaseError => {
-    return new BaseError(name, message, isOperational, statusCode);
+  public static UnauthorizedError = (message?: string): Unauthorized => {
+    return new Unauthorized(message);
   };
 
-  public static NotFoundError = (
-    name: string = 'Not Found Error',
-    message: string = 'Not found.',
-    isOperational: boolean = true,
-    statusCode: HttpStatusCode = HttpStatusCode.NOT_FOUND
-  ): BaseError => {
-    return new BaseError(name, message, isOperational, statusCode);
+  public static NotFoundError = (message?: string): NotFound => {
+    return new NotFound(message);
   };
 
-  public static InternalServerError = (
-    name: string = 'Internal Server Error',
-    message: string = 'Something went wrong.',
-    isOperational: boolean = false,
-    statusCode: HttpStatusCode = HttpStatusCode.INTERNAL_SERVER
-  ): BaseError => {
-    return new BaseError(name, message, isOperational, statusCode);
+  public static InternalServerError = (): InternalServer => {
+    return new InternalServer();
   };
 }
 
