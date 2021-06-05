0663a93a5fb9b4734c300dc691f17148ad96a44d
Utku Çam <utkucam2@gmail.com>
2021-06-05 21:15:55 +0300

    Configure user validation

---
+++ src/validations/user.validations.ts
@@ -0,0 +1,35 @@
+import Joi from 'joi';
+
+const userValidation = {
+  create: {
+    body: Joi.object({
+      email: Joi.string().max(255).email({ allowUnicode: true }).trim().lowercase().required(),
+      password: Joi.string().min(8).max(255).required(),
+    }),
+  },
+  update: {
+    params: Joi.object({
+      id: Joi.string().required(),
+    }),
+    body: Joi.object({
+      email: Joi.string().max(255).email({ allowUnicode: true }).trim().lowercase().required(),
+      password: Joi.string().min(8).max(255).required(),
+    }),
+  },
+  partialUpdate: {
+    params: Joi.object({
+      id: Joi.string().required(),
+    }),
+    body: Joi.object({
+      email: Joi.string().max(255).email({ allowUnicode: true }).trim().lowercase(),
+      password: Joi.string().min(8).max(255),
+    }),
+  },
+  delete: {
+    params: Joi.object({
+      id: Joi.string().required(),
+    }),
+  },
+};
+
+export default userValidation;
