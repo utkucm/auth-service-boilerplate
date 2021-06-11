374b9acb8ce011716b9321652701b742c1e80069
Utku Çam <utkucam2@gmail.com>
2021-06-11 10:26:25 +0300

    Changed users.service to user.service

--- src/middlewares/auth.middlewares.ts
+++ src/middlewares/auth.middlewares.ts
@@ -8,34 +8,32 @@ const options = {
 };
 
 class AuthMiddlewares {
-  public static validateRegister(req: Request, res: Response, next: NextFunction) {
+  public static validateRegister(req: Request, _: Response, next: NextFunction) {
     try {
       logger.info('Validating register body.');
-      const { error, value } = userValidation.create.body.validate(req.body, options);
+      const { error } = userValidation.create.body.validate(req.body, options);
 
       if (error) {
         return next(CreateError.ValidationError(error));
       }
 
-      res.locals.data = value;
-      return next();
+      next();
     } catch (err) {
       logger.error(`Error occurred when validating register body. ERROR: ${err}`);
       next(err);
     }
   }
 
-  public static validateLogin(req: Request, res: Response, next: NextFunction) {
+  public static validateLogin(req: Request, _: Response, next: NextFunction) {
     try {
       logger.info('Validating login body.');
-      const { error, value } = userValidation.login.body.validate(req.body, options);
+      const { error } = userValidation.login.body.validate(req.body, options);
 
       if (error) {
         return next(CreateError.ValidationError(error));
       }
 
-      res.locals.data = value;
-      return next();
+      next();
     } catch (err) {
       logger.error(`Error occurred when validating login body. ERROR: ${err}`);
       next(err);
